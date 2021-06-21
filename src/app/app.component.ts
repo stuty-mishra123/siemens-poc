515358fa93d169ed156955bc9bb7b9598ac21f1f
Stuty Mishra <stuty.mishra@ibm.com>
2021-06-21 21:50:13 +0530

    First commit for siemens poc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LIST1 } from './mock-data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = "Stuty's technical assigment";
+  list1 = LIST1;
+  selectedData: Array<any>= [];
+  List2 : Array<any> = [];
+  selectedData2: Array<any> = [];
+
+  selectList1Data(data){
+    this.selectedData.push(data);
+  }
+  selectList2Data(data){
+    this.selectedData2.push(data);
+  }
+
+  moveToList2(){
+    this.selectedData.forEach(element => {
+      element.selected = false;
+    });
+    this.List2 = this.List2.concat(this.selectedData);
+    this.list1 = this.list1.filter(obj => {
+      return this.List2.indexOf(obj) < 0;
+    });
+
+    this.selectedData = [];
+  }
+  moveToList1(){
+    this.selectedData2.forEach(element => {
+      element.selected = false;
+    });
+    this.list1 = this.list1.concat(this.selectedData2);
+    this.List2 = this.List2.filter(obj1 => {
+      return this.list1.indexOf(obj1) < 0;
+    });
+
+    this.selectedData2 = [];
+  }
 }
---
+++ src/app/mock-data.ts
@@ -0,0 +1,12 @@
+export const LIST1 = [
+    { id: 11, name: 'Dr Nice',selected:false },
+    { id: 12, name: 'Narco',selected:false  },
+    { id: 13, name: 'Bombasto',selected:false  },
+    { id: 14, name: 'Celeritas',selected:false  },
+    { id: 15, name: 'Magneta',selected:false  },
+    { id: 16, name: 'RubberMan',selected:false },
+    { id: 17, name: 'Dynama',selected:false  },
+    { id: 18, name: 'Dr IQ',selected:false  },
+    { id: 19, name: 'Magma',selected:false  },
+    { id: 20, name: 'Tornado',selected:false  }
+  ];
